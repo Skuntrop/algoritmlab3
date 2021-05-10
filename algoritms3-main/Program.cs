54299cf658b7db2348128c2bfbc6744db7869241
Skuntrop <ays02782@gmail.com>
2021-05-10 21:30:30 +0500

    first commit

---
+++ algoritms3-main/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var t1 = new RPN("2*3");         
+            if (t1.ToRPN() != "2,3,*")
+                Console.WriteLine("Test 1 failed");
+
+            var t2 = new RPN("x+y");
+            if (t2.ToRPN() != "x,y,+")
+                Console.WriteLine("Test 2 failed");
+
+            var t3 = new RPN("x*5-2*y");
+            if (t3.ToRPN() != "x,5,*,2,y,*,-")
+                Console.WriteLine("Test 3 failed");
+
+            var t4 = new RPN("x*y=35+5");
+            if (t4.ToRPN() != "x,y,*,35,5,+,=")
+                Console.WriteLine("Test 4 failed");
+
+            var t5 = new RPN("(x-2)*(y+2)");
+            if (t5.ToRPN() != "x,2,-,y,2,+,*")
+                Console.WriteLine("Test 5 failed");
+
+            var t6 = new RPN("x*y=30/2");
+            if (t6.ToRPN() != "x,y,*,30,2,/,=")
+                Console.WriteLine("Test 6 failed");
+
+            var t7 = new RPN("-x+y*2");
+            if (t7.ToRPN() != "-x,y,2,*,+")
+                Console.WriteLine("Test 7 failed");
+
+            var t8 = new RPN("sin(60)");
+            if (t8.ToRPN() != "60,sin")
+                Console.WriteLine("Test 8 failed");
+
+            var t9 = new RPN("sin(60+x)");
+            if (t9.ToRPN() != "60,x,+,sin")
+                Console.WriteLine("Test 9 failed");
+
+            var t10 = new RPN("cos(60*x/2)");
+            if (t10.ToRPN() != "60,x,*,2,/,cos")
+                Console.WriteLine("Test 10 failed");
+
+            var t11 = new RPN("cos(60*-x/2)");
+            if (t11.ToRPN() != "60,-x,*,2,/,cos")
+                Console.WriteLine("Test 11 failed");
+
+            var t12 = new RPN("tan(60-x/2)");
+            if (t12.ToRPN() != "60,x,2,/,-,tan")
+                Console.WriteLine("Test 12 failed");
+
+            var t13 = new RPN("x^y/(5*z)+10");
+            if (t13.ToRPN() != "x,y,^,5,z,*,/,10,+")
+                Console.WriteLine("Test 13 failed");
+        }
+    }
+}
---
+++ algoritms3-main/RPN.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class RPN
+    {
+        private string brackets = "()";
+        private string openBrackets = "(";
+        public string str;
+        private string operations = "+-/*=%sincostanctglog^";
+
+        public RPN(string _str)
+        {
+            str = _str;
+        }
+
+        private int GetOperationsPriority (string op)
+        {
+            return op == "+" || op == "-" ? 1 :
+        op == "*" || op == "/" || op == "%"? 2 :
+        op == "="? 0 :
+        op == "sin"|| op == "cos" || op == "tan" || op == "ctg" || op =="log" || op == "exp"? 4 :
+        op == "^"? 3: -1;
+        }
+
+        private string[] StrToArray(string str)
+        {
+            var result = new List<string>();
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]) || (str[i] == '-' && (i == 0 || openBrackets.Contains(str[i - 1]) || operations.Contains(str[i - 1])) && Char.IsDigit(str[i + 1])))
+                {
+                    var a = str[i].ToString();
+                    if (str.Length != i + 1)
+                    {
+
+
+                        while (Char.IsDigit(str[i + 1]))
+                        {
+                            i++;
+                            a += str[i].ToString();
+                            if (str.Length <= i + 1)
+                                break;
+                        }
+                    }
+                    result.Add(a);
+                }
+                else
+                {
+                    if ((str[i] == '-' && (i == 0 || openBrackets.Contains(str[i - 1]) || operations.Contains(str[i - 1])))
+                                           && !operations.Contains(str[i + 1]))
+                    {
+                        result.Add(str[i].ToString() + str[i + 1].ToString());
+                        i++;
+                    }
+                    else if (i + 4 < str.Length && operations.Contains(str[i].ToString() + str[i + 1].ToString() + str[i + 2].ToString() + str[i + 3].ToString()))
+                    {
+                        i += 4;
+                        result.Add(str[i].ToString() + str[i + 1].ToString() + str[i + 2].ToString() + str[i + 3].ToString());
+                    }
+                    else if (i + 3 < str.Length && operations.Contains(str[i].ToString() + str[i + 1].ToString() + str[i + 2].ToString()) && str[i + 3] == '(')
+                    {
+                        var j = i + 4;
+                        var s = "";
+                        while (str[j] != ')')
+                        {
+                            s += str[j].ToString();
+                            j++;
+                        }
+
+                        var a = new RPN(s);
+                        result.Add(a.ToRPN() + "," + str[i].ToString() + str[i + 1].ToString() + str[i + 2].ToString());
+                        i += j - i + 5;
+                    }
+                    else
+                    {
+                        result.Add(str[i].ToString());
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        public string ToRPN()
+        {
+            var pForm = StrToArray(str);
+            var result = new Stack<string>();
+            var operation = new Stack<string>();
+            foreach(var c in pForm)
+            {
+                if (openBrackets.Contains(c))
+                {
+                    operation.Push(c);
+                }
+                else if (brackets.Contains(c))
+                {
+                    while(!openBrackets.Contains(operation.Peek()))
+                    {
+                        result.Push(operation.Pop());
+                    }
+                    operation.Pop();
+                }
+                else if (operations.Contains(c))
+                {
+
+                    while (operation.Count != 0 && GetOperationsPriority(operation.Peek()) >= GetOperationsPriority(c))
+                        result.Push(operation.Pop());
+                    operation.Push(c);
+                }
+                else
+                {
+                    result.Push(c);
+                }
+            }
+            while(operation.Count != 0)
+            {
+                result.Push(operation.Pop());
+            }
+            return ToStr(result);
+        }
+
+        private string ToStr(Stack<string> res)
+        {
+            var result = new StringBuilder();
+            var array = res.ToArray();
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                result.Append(array[i].ToString());
+                if (i != 0)
+                    result.Append(",");
+            }
+            return result.ToString();
+        }
+    }
+
+}
